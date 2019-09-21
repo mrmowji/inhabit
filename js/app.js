5eeaf1e3e56bc8050e6f22ce96b523fef44c22e8
Mojtaba Javan <javan.mojtaba@gmail.com>
2019-09-21 18:30:53 +0430

    Add raw files

---
+++ js/app.js
@@ -0,0 +1,29 @@
+let MESSAGE_TYPES = {
+  INFO: "info",
+  WARNING: "warning",
+  ERROR: "error",
+  SUCCESS: "success",
+};
+
+persianDate.toLocale('en');
+
+Vue.component('day', {
+  props: ['habit'],
+  template: '',
+});
+
+let app = new Vue({
+  el: "#app",
+  data: {
+    
+  },
+  mounted: function() {
+    
+  },
+  computed: {
+    
+  },
+  methods: {
+    
+  },
+});
